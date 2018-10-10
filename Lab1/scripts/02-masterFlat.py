620b90a3fa4dd5f8f5a021113d98a583a5245424
Abigail Bishop <ms.abigailbishop@gmail.com>
2018-10-10 12:09:35 -0400

    Fixed bad pixel map - now determines hot and dark pixels based on values we determined from the histograms in Lab1/images

--- Lab1/scripts/02-masterFlat.py
+++ Lab1/scripts/02-masterFlat.py
@@ -77,3 +77,5 @@ plt.plot(xgauss, ygauss, color="red", linewidth=1.0)
 plt.plot(xmode, ymode, color="yellow", linewidth=1.0)
 plt.savefig(info['images']+'masterFlat.pdf', ppi=300)
 plt.clf()
+
+files.close()
