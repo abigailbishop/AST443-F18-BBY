3cc5d4673a4ad63d5199cdae114111c856509e37
EmilyBiermann <emily.biermann@stonybrook.edu>
2018-10-16 10:54:03 -0400

    fixed and ran bash sextractor script

--- Lab1/scripts/21-rename_loop.sh
+++ Lab1/scripts/21-rename_loop.sh
@@ -1,3 +1,4 @@
 USE THE FOLLOWING COMMAND TO REMANE:
 
 rename data_. data_ *00*.FIT
+rename .new .fits *.new
--- Lab1/scripts/22-solve_field_loop.sh
+++ Lab1/scripts/22-solve_field_loop.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -u
 
-for file in $(ls)
+for file in $(ls /astrolab/Fall_18/ebiermann/AST443-F18-BBY-data/Lab1/A1-cal_images/*)
 do
-    solve-field --ra 348.5 --dec 8.70 --radius 0.5 ${file}
+    solve-field --ra 348.5 --dec 8.70 --radius 0.5 --continue ${file}
 done
